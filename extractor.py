e3a98bb97ede2532c2530c20deef18b36b92e267
Ignacio López-Francos <iglopezfrancos@gmail.com>
2019-09-16 21:35:46 -0700

    normalize coordenates

--- extractor.py
+++ extractor.py
@@ -1,14 +1,29 @@
 import cv2
 import numpy as np
+np.set_printoptions(suppress=True)
 
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
 
+def add_ones(x):
+  return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+
 class Extractor(object):
-  def __init__(self):
+  def __init__(self, K):
     self.orb = cv2.ORB_create()
     self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
     self.last = None
+    self.K = K
+    self.Kinv = np.linalg.inv(self.K)
+
+  def normalize(self, pts):
+    return np.dot(self.Kinv, add_ones(pts).T).T[:, 0:2]
+
+  def denormalize(self, pt):
+    ret = np.dot(self.K, np.array([pt[0], pt[1], 1.0]))
+    print(ret)
+    #ret /= ret[2]
+    return int(round(ret[0])), int(round(ret[1]))
 
   def extract(self, img):
     # detection
@@ -31,6 +46,11 @@ class Extractor(object):
     # filter
     if len(ret) > 0:
       ret = np.array(ret)
+
+      # normalize coords
+      ret[:, 0, :] = self.normalize(ret[:, 0, :])
+      ret[:, 1, :] = self.normalize(ret[:, 1, :])
+
       model, inliers = ransac((ret[:, 0], ret[:, 1]),
                               FundamentalMatrixTransform,
                               min_samples=8,
